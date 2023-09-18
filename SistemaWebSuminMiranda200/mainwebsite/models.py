75fe4db7938a35d252d688d5c6c5da6bc6063637
Ricardo Amundaray <ricardo1999amundaray@gmail.com>
2023-09-17 21:57:53 -0400

    arreglando models.py, orden de clases

--- SistemaWebSuminMiranda200/mainwebsite/models.py
+++ SistemaWebSuminMiranda200/mainwebsite/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-import proveedores
 
 
 # Create your models here.
@@ -11,56 +10,12 @@ class Cliente(models.Model):
     telefono = models.CharField(max_length=12)
     rif = models.CharField(max_length=15)
 
-class Ordenes(models.Model):
-     id_orden = models.IntegerField(primary_key=True)
-     num_orden = models.IntegerField()
-     fecha_emision = models.DateField()
-     estado = models.CharField(max_length=20)
-     num_factura = models.IntegerField()
-     desc_requisicion = models.CharField(max_length=20)
-     fecha_requisicion = models.DateField()
-     fecha_entrega = models.DateField()
-     solicitado = models.CharField(max_length=25)
-     tlf_solicitado = models.CharField(max_length=12)
-     fecha_ult_mod = models.DateField()
-     num_lote = models.CharField(max_length=20)
-     id_cliente = models.ForeignKey(Cliente, on_delete= models.CASCADE)
-     id_producto = models.ManyToManyField(Inventario, through='Orden_prod')
-     id_destino = models.ForeignKey(Destino, on_delete=models.CASCADE)
-
-
-class Inventario(models.Model):
-   id_producto = models.IntegerField(primary_key=True)
-   nombre_producto = models.CharField(max_length=200)
-   descripcion_prod = models.TextField()
-   cod_producto = models.CharField(max_length=10)
-   precio_unit_ref = models.DecimalField(max_digits=9, decimal_places=3)
-   cant_disponible = models.IntegerField()
-   cant_min = models.IntegerField()
-   cant_max = models.IntegerField()
-   fecha_ult_mod_inv = models.DateField()
-   nota = models.TextField()
-   id_destino = models.ManyToManyField(Destino, through='Prod_Dest')
-   id_proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
-   id_almacen = models.ForeignKey(Almacen, on_delete=models.CASCADE)
-
-
 class Orden_prod(models.Model):
     cantidad = models.IntegerField()
     precio_unit = models.DecimalField
     empaque = models.CharField(max_length=15)
     #Relaciones
   
-class Registro_inventario(models.Model):
-    id_registro = models.IntegerField(primary_key=True)
-    desc_accion = models.CharField(max_length=150)
-    tipo_accion = models.CharField(max_length=150)
-    fecha_accion = models.DateField()
-    cantidad = models.IntegerField()
-    id_producto = models.ForeignKey(Inventario, on_delete=models.CASCADE)
-
-   
-
 
 class Proveedor(models.Model):
     id_proveedor = models.IntegerField(primary_key=True)
@@ -78,12 +33,48 @@ class Destino(models.Model):
     nombre_destino = models.CharField(max_length=200)
     ciudad = models.TextField()
 
+class Inventario(models.Model):
+   id_producto = models.IntegerField(primary_key=True)
+   nombre_producto = models.CharField(max_length=200)
+   descripcion_prod = models.TextField()
+   cod_producto = models.CharField(max_length=10)
+   precio_unit_ref = models.DecimalField(max_digits=9, decimal_places=3)
+   cant_disponible = models.IntegerField()
+   cant_min = models.IntegerField()
+   cant_max = models.IntegerField()
+   fecha_ult_mod_inv = models.DateField()
+   nota = models.TextField()
+   id_destino = models.ManyToManyField(Destino, through='Prod_Dest')
+   id_proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+   id_almacen = models.ForeignKey(Almacen, on_delete=models.CASCADE)
+
+class Registro_inventario(models.Model):
+    id_registro = models.IntegerField(primary_key=True)
+    desc_accion = models.CharField(max_length=150)
+    tipo_accion = models.CharField(max_length=150)
+    fecha_accion = models.DateField()
+    cantidad = models.IntegerField()
+    id_producto = models.ForeignKey(Inventario, on_delete=models.CASCADE)
+
 class Prod_Dest(models.Model):
     id_producto = models.ForeignKey(Inventario, on_delete=models.CASCADE)
     id_destino = models.ForeignKey(Destino, on_delete=models.CASCADE)
     consumo_prom_dia = models.IntegerField()
     fecha_ult_sumin = models.DateField()
 
-
-
-
+class Ordenes(models.Model):
+     id_orden = models.IntegerField(primary_key=True)
+     num_orden = models.IntegerField()
+     fecha_emision = models.DateField()
+     estado = models.CharField(max_length=20)
+     num_factura = models.IntegerField()
+     desc_requisicion = models.CharField(max_length=20)
+     fecha_requisicion = models.DateField()
+     fecha_entrega = models.DateField()
+     solicitado = models.CharField(max_length=25)
+     tlf_solicitado = models.CharField(max_length=12)
+     fecha_ult_mod = models.DateField()
+     num_lote = models.CharField(max_length=20)
+     id_cliente = models.ForeignKey(Cliente, on_delete= models.CASCADE)
+     id_producto = models.ManyToManyField(Inventario, through='Orden_prod')
+     id_destino = models.ForeignKey(Destino, on_delete=models.CASCADE)
