e78562da66223457edc35649d2cf779761590f9b
Ricardo A <ricardoamundaray1999@gmail.com>
2025-01-08 10:49:10 -0400

    arreglando credenciales de envio de correo | creando logica de recuperación de contraseña via email | cambiando localización de sistema a español.

--- root/mainwebsite/urls.py
+++ root/mainwebsite/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('login/', views.login_user, name="login"),
     path('logout/', views.logout_user, name="logout"),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='reset_password'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('password_reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
--- root/mainwebsite/views.py
+++ root/mainwebsite/views.py
@@ -733,8 +733,8 @@ def prod_dest_eliminar(request, pk):
 #SERVICIOS RELACIONADOS CON SESIONES DE USUARIOS
 def login_user(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user = authenticate(request,username=username, password=password)
         if user is not None:
             login(request, user)
